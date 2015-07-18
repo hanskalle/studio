03769f37aa796b7227838bf1b774c453b30414b7
Hans Kalle <hans.kalle@gmail.com>
2015-07-18 19:51:12 +0200

    Refactoring and cleanup

--- planner/generateschedule.py
+++ planner/generateschedule.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
-from datetime import date, timedelta
-import json, requests
+import json
 from datetime import datetime, date, timedelta, time
+
+import requests
+
 host = 'www.ichthusculemborg.nl'
 auth = ('username', 'password')
+state_value = {'no': '0', 'yes': '1', 'maybe': '.5'}
 
 def create_event(date, time, description, location, remark):
     url = "http://"+host+"/services/events"
@@ -43,7 +46,7 @@ def get_persons():
     response = conn.getresponse()
     if response.status == 302:
         response.read()
-        location = response.getheader('location');
+        location = response.getheader('location')
         headers = {"Content-type": "application/x-www-form-urlencoded",
                    "Accept": "text/plain",
                    "Cookie": response.getheader('set-cookie')}
@@ -70,17 +73,15 @@ def get_availability(person, taskname):
     for task in person['tasks']:
         if task['task'] == taskname:
             total = 0
-            state_value = { 'no': 0.0, 'yes': 1.0, 'maybe': 0.5 }
             for week in task['availability']:
                 total += state_value[week['state']]
             if total > 0.0:
-                return task['availability'];
+                return task['availability']
             else:
                 return None
     return None
 
 def write_beschikbaarheid(filename, persons):
-    state_value = { 'no': '0', 'yes': '1', 'maybe': '.5' }
     file = open(filename, 'w')
     weeks = get_weeks(persons)
     file.write('param first_week := ' + weeks[0] + ';\n')
@@ -92,12 +93,12 @@ def write_beschikbaarheid(filename, persons):
             file.write(week)
         file.write(':=\n')        
         for person in sorted(persons):
-            availability = get_availability(person, task);
+            availability = get_availability(person, task)
             if availability != None:
                 file.write(person['name'].replace(' ', '_'))
                 for available in availability:
                     file.write(' ')
-                    file.write(state_value[available['state']]);
+                    file.write(state_value[available['state']])
                 file.write('\n')
         file.write(';\n')
     file.write('end;\n')
@@ -105,21 +106,35 @@ def write_beschikbaarheid(filename, persons):
     
 def parse_results(filename):
     rooster = {}
+    week = 0
     for line in open(filename):
         if 'w = ' in line:
             week = line.strip()[4:]
-            if not week in rooster:
-                rooster[week] = {}
-                rooster[week]['datum'] = date(2015,1,4) + timedelta((int(week)-1) * 7) 
+            add_week(rooster, week)
         elif ' = ' in line:
             fields = line.strip().split(' = ')
             task = fields[0][0:-1].replace('_', ' ')
             person = fields[1].replace('_', ' ')
-            if task in rooster[week]:
-                rooster[week][task] += ', ' + person
-            else:
-                rooster[week][task] = person
+            add_person(rooster, week, task, person)
     return rooster
+
+
+def add_week(rooster, week):
+    if not week in rooster:
+        rooster[week] = {}
+        rooster[week]['datum'] = getDateOfSundayOfWeek(week)
+
+
+def getDateOfSundayOfWeek(week):
+    return date(2015, 1, 4) + timedelta((int(week) - 1) * 7)
+
+
+def add_person(rooster, week, task, person):
+    if task in rooster[week]:
+        rooster[week][task] += ', ' + person
+    else:
+        rooster[week][task] = person
+
 
 def get_results(timlim):
     import subprocess
@@ -156,8 +171,13 @@ def write_markup(filename, rooster):
             file.write(','.join(remarks))
         file.write(' |\n')
     file.close()
+    show_file(filename)
+
+
+def show_file(filename):
     for line in open(filename, 'r'):
         print line[0:-1]
+
 
 def write_rest(rooster):
     columns = {
